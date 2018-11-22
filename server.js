78347859b9ba747ab17eeed6299a3634479dd20e
ronak <knockronak@gmail.com>
2018-11-22 16:25:02 -0500

    changing the project structure for heroku deployment

--- server.js
+++ server.js
@@ -25,11 +25,10 @@ mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname,'../..', 'client/build')));
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../..', 'client/build', 'index.html'));
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 }
 
